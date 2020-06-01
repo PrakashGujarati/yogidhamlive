30249503409eaed6d70778e1c0a6d3c771568b00
Prakash Gujarati <prakash@vsc.edu.in>
2020-06-01 15:25:01 +0530

    url update fb

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-header("Location: https://youtu.be/RogyPOhM9k8");
+//header("Location: https://youtu.be/RogyPOhM9k8");
+header("Location: https://www.facebook.com/351359128532838/videos/1582970628545952");
 exit();
 ?>
